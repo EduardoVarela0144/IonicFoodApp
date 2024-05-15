cbeecb75aa2d72205c90f8513db339edeca7a5dd
EduardoVarela0144 <eduardo.varela142001@gmail.com>
2024-05-14 20:33:50 -0600

    Categories page

--- src/app/pages/categorias/categorias.page.ts
+++ src/app/pages/categorias/categorias.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, LoadingController } from '@ionic/angular';
+import { AddCategoryComponent } from 'src/app/components/add-category/add-category.component';
+
+import { CategoryService } from 'src/app/service/category.service';
 
 @Component({
   selector: 'app-categorias',
@@ -7,9 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriasPage implements OnInit {
 
-  constructor() { }
+  Categories: any[] = [];
+  loading: any;
+
+  constructor(
+    private allCategory: CategoryService,
+    private modalCtrl: ModalController,
+    private loadingCtrl: LoadingController
+  ) {}
+
+  async presentLoading() {
+    this.loading = await this.loadingCtrl.create({
+      message: 'Cargando...',
+    });
+    await this.loading.present();
+  }
+
+  async dismissLoading() {
+    if (this.loading) {
+      await this.loading.dismiss();
+    }
+  }
 
   ngOnInit() {
+    this.presentLoading();
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.allCategory.getCategorias().subscribe((res: any) => {
+      this.Categories = res.categorias;
+      this.dismissLoading();
+    });
+  }
+
+  async formCategory() {
+    const modal = await this.modalCtrl.create({
+      component: AddCategoryComponent,
+      initialBreakpoint: 0.8,
+    });
+    modal.onDidDismiss().then(() => {
+      this.presentLoading();
+      this.getCategories();
+      this.dismissLoading();
+    });
+    return await modal.present();
   }
 
 }
