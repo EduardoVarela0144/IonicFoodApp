fbb2e6ddc09c07b7fe746eed0da10e7b4d315191
EduardoVarela0144 <eduardo.varela142001@gmail.com>
2024-05-30 17:19:08 -0600

    update service

--- src/app/service/food.service.ts
+++ src/app/service/food.service.ts
@@ -13,12 +13,12 @@ export class FoodService {
   ) { }
 
   getFoods() {
-    return this.http.get(apiUrl + 'food');
+    return this.http.get(apiUrl + 'products');
   }
 
   addFood(data: any) {
     console.log(data);
-    return this.http.post(apiUrl + 'food', data);
+    return this.http.post(apiUrl + 'products', data);
   }
 
 }
